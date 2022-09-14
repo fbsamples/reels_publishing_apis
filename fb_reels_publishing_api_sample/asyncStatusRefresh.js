7a99760fc7f8635fad824b6ab9c48fd35d86ff67
Dylan Parent <35587085+dylanp12@users.noreply.github.com>
2022-09-14 10:29:00 -0400

    Automated Status refresh
    Async refresh updates status text in the background and terminates once status has reached completed then redirects to final page.

---
+++ fb_reels_publishing_api_sample/asyncStatusRefresh.js
@@ -0,0 +1,23 @@
+
+
+/* onload call fetchStatus then based on response update ui accordingly*/
+const asyncInterval = setInterval(fetchStatus, 1000);
+
+function fetchStatus() {
+  jQuery.get('/asyncStatus', function(data, status) {
+    updateStatus(data.status);
+    if (data.status == 'complete') {
+      clearInterval(asyncInterval);
+      document.getElementById('check-status-button').click();
+    }
+  });
+}
+
+function updateStatus(phase) {
+  console.log('phase: ' + phase);
+  const ele = document.getElementById('status');
+
+  if (ele) {
+    ele.innerHTML = 'Status: ' + phase;
+  }
+}
