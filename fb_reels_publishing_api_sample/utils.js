3571c623dbabd3f5ce59edeba5c96fe74638b092
Karthika Krishnan <kaykrish@fb.com>
2022-09-15 16:50:58 -0700

    Front-end validation for date-time picker

--- fb_reels_publishing_api_sample/utils.js
+++ fb_reels_publishing_api_sample/utils.js
@@ -3,6 +3,8 @@
 //  This source code is licensed under the license found in the
 //  LICENSE file in the root directory of this source tree.
 
+const PUBLISH_TIME_INTERVAL = 600;
+
 function convertToUnix(raw_date){
     const [dateComponents, timeComponents] = raw_date.split('T');
 
@@ -12,6 +14,25 @@ function convertToUnix(raw_date){
     const date = new Date(+year, month - 1, +day, +hours, +minutes);
 
     return Math.floor(date.getTime() / 1000)
+}
+
+function getDifference(scheduled_publish_time, current_time_in_seconds) {
+    return Math.abs(scheduled_publish_time - current_time_in_seconds);
+}
+
+function check_time_difference(publish_time) {
+    let publish_time_temp = document.getElementById(publish_time).value;
+    const scheduled_publish_time = convertToUnix(publish_time_temp);
+    // If publish time is within the next 10 minutes, alert
+    // Get Current time
+    const current_time = Date.now();
+    // Current Time in Seconds
+    const current_time_in_seconds = Math.floor(current_time / 1000);
+
+    if (getDifference(scheduled_publish_time, current_time_in_seconds) < PUBLISH_TIME_INTERVAL ) {
+        alert('Publish Time is within 10 minute interval. Please select a different date');
+        document.getElementById(publish_time).value = '';
+    }
 }
 
 module.exports = convertToUnix;
