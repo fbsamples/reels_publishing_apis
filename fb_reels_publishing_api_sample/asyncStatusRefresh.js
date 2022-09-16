04670ea540c78748deb10367164114e6433bf6e8
Steve Kwak <stevekwak@fb.com>
2022-09-16 17:30:35 -0400

    copyright added

--- fb_reels_publishing_api_sample/asyncStatusRefresh.js
+++ fb_reels_publishing_api_sample/asyncStatusRefresh.js
@@ -1,3 +1,8 @@
+//  Copyright (c) Meta Platforms, Inc. and affiliates.
+//  All rights reserved.
+//  This source code is licensed under the license found in the
+//  LICENSE file in the root directory of this source tree.
+
 /* onload call fetchStatus then based on response update ui accordingly*/
 const asyncInterval = setInterval(fetchStatus, 3500);
 
